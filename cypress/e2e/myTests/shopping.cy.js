f3151b2614b452f7e11bc97b4980c1d7f138ec33
Lexi Mejias <104792866+Amejias722@users.noreply.github.com>
2024-06-28 11:09:39 -0400

    fixed tests

--- cypress/e2e/myTests/shopping.cy.js
+++ cypress/e2e/myTests/shopping.cy.js
@@ -3,8 +3,12 @@ import { contactInfo } from "../../support/contact";
 
 describe('E2E tests for e-commerce site', () => {
 
+  beforeEach(() => {
+    cy.visit('https://www.demoblaze.com/')
+  });
+  
+  
     it('User can login', () => {
-      cy.visit('https://www.demoblaze.com/')
       cy.get('[id="login2"]').click()
       cy.get('[id="logInModalLabel"]').should('be.visible')
       cy.get('[id="loginusername"').type('QA Tester')
@@ -13,15 +17,15 @@ describe('E2E tests for e-commerce site', () => {
       cy.intercept('POST', '/login', (req) => {
         req.reply((res) => {
           expect(res.statusCode).to.eq(200);
-        });
+       });
       });
       
       
     });
 
     it('User can checkout item', () => {
-      cy.visit('https://www.demoblaze.com/')
-      cy.get('a[href="prod.html?idp_=1"]').first().click()
+     
+      cy.get('[src="imgs/galaxy_s6.jpg"]').first().click()
       cy.get('a[href="#"]').contains('Add to cart').click();
       cy.get('a[href="cart.html"]').contains('Cart').click();
       cy.get('button').contains('Place Order').click();
@@ -38,9 +42,3 @@ describe('E2E tests for e-commerce site', () => {
      
     
   })
-
-
-
-
-
-
