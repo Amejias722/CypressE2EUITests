53f9cf7122b6c6f198080142584ece526014ee56
Lexi Mejias <104792866+Amejias722@users.noreply.github.com>
2024-08-29 15:12:51 -0400

    added api test example

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
+// cypress.config.js
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    supportFile: 'cypress/support/e2e.js',
+    // Other configuration options
   },
 });
---
+++ cypress/e2e/myTests/apiTests.cy.js
@@ -0,0 +1,40 @@
+
+describe('URL Check', () => {
+    it('should return a 200 status for HOMEPAGE', () => {
+      cy.request('GET', 'https://hellowisp.com')
+        .its('status')
+        .should('equal', 200);
+    });
+
+    describe('API Test - verify footer links', () => {
+        it('should return the correct response for each endpoint', () => {
+          const base = 'https://hellowisp.com/shop';
+          const endpoints = [
+            { url: '/reproductive-health', expectedStatusCode: 200, expectedText: 'Reproductive Health' },
+            { url: '/herpes', expectedStatusCode: 200, expectedText: 'Herpes' },
+            { url: '/wellness-essentials', expectedStatusCode: 200, expectedText: 'Wellness Essentials' },
+            { url: '/prevention', expectedStatusCode: 200, expectedText: 'Prevention' },
+            { url: '/std', expectedStatusCode: 200, expectedText: 'STD' },
+            { url: '/fertility', expectedStatusCode: 200, expectedText: 'Fertility' },
+            { url: '/wisp-care', expectedStatusCode: 200, expectedText: 'Wispcare' },
+
+          ];
+      
+          endpoints.forEach(endpoint => {
+            cy.request({
+              url: `${base}${endpoint.url}`,
+              failOnStatusCode: false // Allows for custom status code checks
+            }).then(response => {
+              // Assert that the status code matches the expected value
+              expect(response.status).to.eq(endpoint.expectedStatusCode);
+              expect(response.headers['content-type']).to.include('text/html');
+              expect(response.body).to.contain(endpoint.expectedText);
+
+            });
+          });
+        });
+      });
+      
+      
+      
+  });
