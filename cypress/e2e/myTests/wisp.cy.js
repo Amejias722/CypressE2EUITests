8acc2121c5a450af3ff14f616efd34b0774aa026
Lexi Mejias <104792866+Amejias722@users.noreply.github.com>
2024-08-28 22:11:35 -0400

    added new test

---
+++ cypress/e2e/myTests/wisp.cy.js
@@ -0,0 +1,54 @@
+import { describe } from "mocha";
+
+
+const ids = {
+    acceptCookies: '[id="onetrust-accept-btn-handler"]',
+    planBImage: '[alt="Hands holding Plan B and Ella pills on a pink background"]',
+    planBimage2: '[alt="Ella emergency contraception in foil packet to prevent pregnancy on a red surface, on a pink background"]',
+    puaPharmacy: '[data-cy="product-flow-option-button-EU-OT-LP"]',
+    nextButton: '[data-cy="navigation-buttons-next"]',
+    noThanksoption: '[data-cy="product-flow-option-button-unknown"]',
+    checkoutButton: '[data-cy="cart-checkout-click"]',
+    }
+
+describe("Wisp E2E Checkout", () => {
+    beforeEach(() => {
+        cy.visit("https://hellowisp.com")  
+    });
+it('User can add item to cart and checkout', () => {
+
+    cy.get(ids.acceptCookies, { timeout: 10000 })
+    .should('be.visible')
+    .click();
+
+    cy.step('User selects Shop Treatments Button')
+    cy.get('[class="button-secondary font-medium"]').contains('Shop Treatments').click()
+    cy.url().should('include', 'https://hellowisp.com/shop-home')
+
+    cy.step('User Selects Option')
+    cy.contains('Reproductive Health').scrollIntoView()
+    cy.get(ids.planBImage).click()
+    cy.url().should('include','/emergency-contraception')
+    cy.get(ids.planBimage2).click()
+    cy.url().should('include', '/products/ella')
+
+    cy.step('User goes through checkout flow')
+    cy.get('button').contains('Get Started').click()
+    cy.url().should('include', 'ella?flow=open')
+    cy.get(ids.puaPharmacy).click()
+    cy.get(ids.nextButton).click();
+
+    for (let i = 0; i < 4; i++) {
+    cy.get(ids.noThanksoption).click();
+    cy.get(ids.nextButton).click();
+    cy.wait(2000);
+    }
+
+  cy.get('button').contains('Add to Cart').should('be.visible').click();
+  cy.url().should('include', "https://hellowisp.com/cart");
+  cy.get(ids.checkoutButton).should('be.visible');
+
+});
+
+
+});
---
+++ cypress/fixtures/functions_buttons.js
@@ -0,0 +1,13 @@
+
+// const ids = {
+// acceptCookies: '[id="onetrust-accept-btn-handler"]',
+// noThanksBtn: '["product-flow-option-button-unknown"]',
+// nextBtn: '[data-cy="navigation-buttons-next"]'
+
+
+
+
+
+// }
+// module.exports = ids;
+
